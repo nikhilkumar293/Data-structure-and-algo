3000c8715f6e63b5e74e69f17f2e52183d9dedca
nikhilkumar293 <71305499+nikhilkumar293@users.noreply.github.com>
2023-01-01 16:08:24 +0530

    Add files via upload

---
+++ recursion/Factorial.java
@@ -0,0 +1,18 @@
+package recursion;
+import java.util.*;
+public class Factorial {
+	public static void main(String args[]) {
+		System.out.println(fact(5));
+	}
+	public static int fact(int n) {
+		
+		 if(n<1) {
+			 return -1;
+		 }
+		 if(n==0 || n==1) {
+			 return 1;
+		 }
+		return n*fact(n-1);
+	}
+
+}
